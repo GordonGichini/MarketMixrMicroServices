b0c8444cbab37816e20cfa57d3fafb6857e5a3ea
Gordon <gordongichini1@gmail.com>
2024-01-01 01:52:58 +0300

    Mail Service implemented

--- MailService/Program.cs
+++ MailService/Program.cs
@@ -1,3 +1,10 @@
+using MailService.Data;
+using MailService.Extensions;
+using MailService.Messaging;
+using MailService.Service;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +13,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("myconnection"));
+});
+
+//changing the above service to SingleTon
+var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("myconnection"));
+builder.Services.AddSingleton(new EmailService(optionsBuilder.Options));
+
+//
+builder.Services.AddSingleton<IAzureServiceBusConsumer, AzureServiceBusConsumer>();
+
 
 var app = builder.Build();
 
@@ -17,6 +38,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.useAzure();
 
 app.UseAuthorization();
 
