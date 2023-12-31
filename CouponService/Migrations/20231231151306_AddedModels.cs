74c7ab327c7878fa4c790ff8ec846855fbd9b1c8
Gordon <gordongichini1@gmail.com>
2023-12-31 21:33:40 +0300

    coupon service

---
+++ CouponService/Migrations/20231231151306_AddedModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CouponService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CouponCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CouponAmount = table.Column<int>(type: "int", nullable: false),
+                    CouponMinAmount = table.Column<int>(type: "int", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
