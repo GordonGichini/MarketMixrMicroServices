35ace4c29dac53407a122eebe35fa934797c2ad5
Gordon <gordongichini1@gmail.com>
2024-01-01 17:36:35 +0300

    Product Service done

--- ProductService/Program.cs
+++ ProductService/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using ProductService.Data;
+using ProductService.Extensions;
+using ProductService.Services;
+using ProductService.Services.IServices;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//custom
+builder.AddSwaggenGenExtension();
+builder.AddAuth();
+//db connection
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("myconnection"));
+});
+//Reg for DI
+builder.Services.AddScoped<IProduct, ProductServices>();
+builder.Services.AddScoped<IImage, ImageServices>();
+//automapper 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 
 var app = builder.Build();
 
@@ -17,6 +40,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseMigrations();
 
 app.UseAuthorization();
 
